05c2476a0cadce7c1aeac95e6553cfb07a8d74f1
janani <janani@janani-Vostro-3446.(none)>
2014-07-22 10:47:06 +0530

    potential

--- scipy/special/tests/test_ellip_harm.py
+++ scipy/special/tests/test_ellip_harm.py
@@ -7,47 +7,25 @@ from __future__ import division, print_function, absolute_import
 
 import numpy as np
 from numpy.testing import assert_equal, assert_almost_equal, assert_allclose
+from scipy.special._testutils import assert_func_equal
 from scipy.special import ellip_harm, ellip_harm_2, ellip_normal
 from scipy.integrate import quad
 from numpy import array, sqrt, pi
 from scipy.special._testutils import FuncData
 
-def ellip_norm_integral(h2, k2, n, p):
-    h = sqrt(h2)
-    k = sqrt(k2)
-    def func1(t):
-        t2 = t*t
-        i = ellip_harm( h2, k2, n, p, t)
-        result = i*i/sqrt((t + h)*(t + k))
-        return result
-
-    def func2(t):
-        t2 = t*t
-        i = ellip_harm( h2, k2, n, p, t)
-        result = t2*i*i/sqrt((t + h)*(t + k))
-        return result
-
-    def func3(t):
-        t2 = t*t
-        i = ellip_harm( h2, k2, n, p, t)
-        result = i*i/sqrt((t + h)*(k2 - t2))
-        return result
-
-    def func4(t):
-        t2 = t*t
-        i = ellip_harm( h2, k2, n, p, t)
-        result = t2*i*i/sqrt((t + h)*(k2 - t2))
-        return result
-
-    res1, err1 = quad(func1, h, k, epsabs=1e-08, epsrel=1e-15, weight="alg", wvar=(-0.5, -0.5))
-    res2, err2 = quad(func2, h, k, epsabs=1e-08, epsrel=1e-15, weight="alg", wvar=(-0.5, -0.5))
-    res3, err3 = quad(func3, 0, h, epsabs=1e-08, epsrel=1e-15, weight="alg", wvar=(0, -0.5))
-    res4, err4 = quad(func4, 0, h, epsabs=1e-08, epsrel=1e-15, weight="alg", wvar=(0, -0.5))
-    error = 8*(res3*err2 + err3*res2 + res1*err4 + res4*err1)
-    return 8*(res2*res3 - res1*res4), error
+def test_ellip_norm1():
+    def trial(r1, r2):
+        res = 1/(4*pi*1.602*10**-19*(r2 - r1))
+        return res
+    sum1 =0
+    print('hi') 
+    for n in range(10):
+        for p in range(1, 2*n+1):
+            sum1 += 4*pi*ellip_harm(10,15,n,p,80)*ellip_harm_2(10,15,n,p,70)/((2*n + 1)*ellip_normal(10,15,n,p))
+    assert_almost_equal(sum1, trial(80,70))
         
 def test_ellip_norm():
-
+    
     def G01(h2, k2):
         return 4*pi
 
@@ -61,11 +39,11 @@ def test_ellip_norm():
         return 4*pi*k2*(k2 - h2)/3
 
     def G22(h2, k2):
-        res = 2*(h2**4 + k2**4) - 4*h2*k2*(h2**2 + k2**2) + 6*h2**2*k2**2 + sqrt(h2**2 + k2**2 - h2*k2)*(-2*(h2**3 + k2**3) + 3*h2*k2*(h2 + k2) ) 
+        res = 2*(h2**4 + k2**4) - 4*h2*k2*(h2**2 + k2**2) + 6*h2**2*k2**2 + sqrt(h2**2 + k2**2 - h2*k2)*(-2*(h2**3 + k2**3) + 3*h2*k2*(h2 + k2)) 
         return 16*pi/405*res
 
     def G21(h2, k2):
-        res = 2*(h2**4 + k2**4) - 4*h2*k2*(h2**2 + k2**2) + 6*h2**2*k2**2 + sqrt(h2**2 + k2**2 - h2*k2)*(2*(h2**3 + k2**3) - 3*h2*k2*(h2 + k2) ) 
+        res = 2*(h2**4 + k2**4) - 4*h2*k2*(h2**2 + k2**2) + 6*h2**2*k2**2 + sqrt(h2**2 + k2**2 - h2*k2)*(2*(h2**3 + k2**3) - 3*h2*k2*(h2 + k2)) 
         return 16*pi/405*res
 
     def G23(h2, k2):
@@ -104,74 +82,60 @@ def test_ellip_norm():
     def G37(h2, k2):
         return 4*pi*h2**2*k2**2*(k2 - h2)**2/105
 
-    data = [
-        (5,8,0,1, G01(5, 8)),
-        (5,8,1,1, G11(5, 8)),
-        (5,8,1,2, G12(5, 8)),
-        (5,8,1,3, G13(5, 8)),
-        (5,8,2,1, G21(5, 8)),
-        (5,8,2,2, G22(5, 8)),
-        (5,8,2,3, G23(5, 8)),
-        (5,8,2,4, G24(5, 8)),
-        (5,8,2,5, G25(5, 8)),
-        (5,8,3,1, G31(5, 8)),
-        (5,8,3,2, G32(5, 8)),
-        (5,8,3,3, G33(5, 8)),
-        (5,8,3,4, G34(5, 8)),
-        (5,8,3,5, G35(5, 8)),
-        (5,8,3,6, G36(5, 8)),
-        (5,8,3,7, G37(5, 8)),
-        (6.25,36,0,1, G01(6.25, 36)),
-        (6.25,36,1,1, G11(6.25, 36)),
-        (6.25,36,1,2, G12(6.25, 36)),
-        (6.25,36,1,3, G13(6.25, 36)),
-        (6.25,36,2,1, G21(6.25, 36)),
-        (6.25,36,2,2, G22(6.25, 36)),
-        (6.25,36,2,3, G23(6.25, 36)),
-        (6.25,36,2,4, G24(6.25, 36)),
-        (6.25,36,2,5, G25(6.25, 36)),
-        (6.25,36,3,1, G31(6.25, 36)),
-        (6.25,36,3,2, G32(6.25, 36)),
-        (6.25,36,3,3, G33(6.25, 36)),
-        (6.25,36,3,4, G34(6.25, 36)),
-        (6.25,36,3,5, G35(6.25, 36)),
-        (6.25,36,3,6, G36(6.25, 36)),
-        (6.25,36,3,7, G37(6.25, 36)),
-        ]
+    assert_almost_equal(ellip_normal(5,8,0,1), G01(5, 8))
+    assert_almost_equal(ellip_normal(5,8,1,1), G11(5, 8))
+    assert_almost_equal(ellip_normal(5,8,1,2), G12(5, 8))
+    assert_almost_equal(ellip_normal(5,8,1,3), G13(5, 8))
+    assert_almost_equal(ellip_normal(5,8,2,1), G21(5, 8))
+    assert_almost_equal(ellip_normal(5,8,2,2), G22(5, 8))
+    assert_almost_equal(ellip_normal(5,8,2,3), G23(5, 8))
+    assert_almost_equal(ellip_normal(5,8,2,4), G24(5, 8))
+    assert_almost_equal(ellip_normal(5,8,2,5), G25(5, 8))
+    assert_almost_equal(ellip_normal(5,8,3,1), G31(5, 8))
+    assert_almost_equal(ellip_normal(5,8,3,2), G32(5, 8))
+    assert_almost_equal(ellip_normal(5,8,3,3), G33(5, 8))
+    assert_almost_equal(ellip_normal(5,8,3,4), G34(5, 8))
+    assert_almost_equal(ellip_normal(5,8,3,5), G35(5, 8))
+    assert_almost_equal(ellip_normal(5,8,3,6), G36(5, 8))
+    assert_almost_equal(ellip_normal(5,8,3,7), G37(5, 8))
+    assert_almost_equal(ellip_normal(10.14,18.432,0,1), G01(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,1,1), G11(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,1,2), G12(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,1,3), G13(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,2,1), G21(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,2,2), G22(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,2,3), G23(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,2,4), G24(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,2,5), G25(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,3,1), G31(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,3,2), G32(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,3,3), G33(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,3,4), G34(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,3,5), G35(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,3,6), G36(10.14, 18.432))
+    assert_almost_equal(ellip_normal(10.14,18.432,3,7), G37(10.14, 18.432))
 
-    data = array(data, dtype=float)
+#    known_funcs = {(1, 1): G11, (1, 2): G12, (1, 3): G13, (2, 1): G21, (2, 2): G22, (2, 3): G23, (2, 4): G24, (2,5): G25, (3, 1): G31, (3, 2): G32, (3, 3): G33, (3, 4): G34, (3, 5): G35, (3, 6): G36, (3, 7): G37 }
+#    point_ref =[]
+#    def ellip_normal_known(h2, k2, n, p):
+#        for i in range(h2.size):
+#            func = known_funcs[(int(n[i]), int(p[i]))]
+#            point_ref.append(func(h2[i], k2[i]))
+#        return point_ref
 
-    def w(a, b, c, d):
-        return ellip_normal(a, b, c, d)
+# generate both large and small h2 < k2 pairs
+#    np.random.seed(1234)
+#    h2 = np.random.pareto(0.5, size=30)
+#    k2 = h2 * (1 + np.random.pareto(0.5, size=h2.size))
 
-    olderr = np.seterr(all='ignore')
+#    points = []
+#    for i in range(h2.size):
+#        for n in range(4):
+#            for p in range(1, 2*n+2):
+#               points.append((h2[i], k2[i], int(n), int(p)))
+#    points = np.array(points
+#    assert_func_equal(ellip_normal, ellip_normal_known, points)
 
-    integral_result, total_err = ellip_norm_integral(5, 8, 4, 1)
-    assert_allclose(ellip_normal(5,8,4,1), integral_result, atol=10*total_err)
-    integral_result, total_err = ellip_norm_integral(5, 8, 4, 2)
-    assert_allclose(ellip_normal(5,8,4,2), integral_result, atol=10*total_err)
-    integral_result, total_err = ellip_norm_integral(5, 8, 4, 3)
-    assert_allclose(ellip_normal(5,8,4,3), integral_result, atol=10*total_err)
-    integral_result, total_err = ellip_norm_integral(5, 8, 4, 4)
-    assert_allclose(ellip_normal(5,8,4,4), integral_result, atol=10*total_err)
-    integral_result, total_err = ellip_norm_integral(5, 8, 4, 5)
-    assert_allclose(ellip_normal(5,8,4,5), integral_result, atol=10*total_err)
-    integral_result, total_err = ellip_norm_integral(5, 8, 4, 6)
-    assert_allclose(ellip_normal(5,8,4,6), integral_result, atol=10*total_err)
-    integral_result, total_err = ellip_norm_integral(5, 8, 4, 7)
-    assert_allclose(ellip_normal(5,8,4,7), integral_result, atol=10*total_err)
-    integral_result, total_err = ellip_norm_integral(5, 8, 4, 8)
-    assert_allclose(ellip_normal(5,8,4,8), integral_result, atol=10*total_err)
-    integral_result, total_err = ellip_norm_integral(5, 8, 4, 9)
-    assert_allclose(ellip_normal(5,8,4,9), integral_result, atol=10*total_err)
-    integral_result, total_err = ellip_norm_integral(5, 8, 5, 5)
-    assert_allclose(ellip_normal(5,8,5,5), integral_result, atol=10*total_err)
-   
-    try:
-        FuncData(w, data, (0,1,2,3), 4, rtol=1e-10, atol=1e-13).check()
-
-    finally:
-        np.seterr(**olderr)
 def test_ellip_harm_2():
 
     def I1(h2, k2, s):
@@ -234,55 +198,27 @@ def test_ellip_harm():
 
     def E37(h2, k2, s):
         return s * sqrt(abs((s*s - h2)*(s*s - k2)))
-
    
     assert_equal(ellip_harm(5,8,1,2,2.5,1,1), ellip_harm(5,8,1,2,2.5))
 
+    known_funcs = {(0, 1): E01, (1, 1): E11, (1, 2): E12, (1, 3): E13, (2, 1): E21, (2, 2): E22, (2, 3): E23, (2, 4): E24, (2,5): E25, (3, 1): E31, (3, 2): E32, (3, 3): E33, (3, 4): E34, (3, 5): E35, (3, 6): E36, (3, 7): E37 }
+    point_ref =[]
+    def ellip_harm_known(h2, k2, n, p, s):
+        for i in range(h2.size):
+            func = known_funcs[(int(n[i]), int(p[i]))]
+            point_ref.append(func(h2[i], k2[i], s[i]))
+        return point_ref
 
-    data = [
-        (5,8,0,1,2.5,1,1, E01(5, 8, 2.5)),
-        (5,8,1,1,2.5,1,1, E11(5, 8, 2.5)),
-        (5,8,1,2,2.5,1,1, E12(5, 8, 2.5)),
-        (5,8,1,3,2.5,1,1, E13(5, 8, 2.5)),
-        (5,8,2,1,2.5,1,1, E21(5, 8, 2.5)),
-        (5,8,2,2,2.5,1,1, E22(5, 8, 2.5)),
-        (5,8,2,3,2.5,1,1, E23(5, 8, 2.5)),
-        (5,8,2,4,2.5,1,1, E24(5, 8, 2.5)),
-        (5,8,2,5,2.5,1,1, E25(5, 8, 2.5)),
-        (5,8,3,1,2.5,1,1, E31(5, 8, 2.5)),
-        (5,8,3,2,2.5,1,1, E32(5, 8, 2.5)),
-        (5,8,3,3,2.5,1,1, E33(5, 8, 2.5)),
-        (5,8,3,4,2.5,1,1, E34(5, 8, 2.5)),
-        (5,8,3,5,2.5,1,1, E35(5, 8, 2.5)),
-        (5,8,3,6,2.5,1,1, E36(5, 8, 2.5)),
-        (5,8,3,7,2.5,1,1, E37(5, 8, 2.5)),
-        (6.25,36,0,1,7.3,1,1, E01(6.25, 36, 7.3)),
-        (6.25,36,1,1,7.3,1,1, E11(6.25, 36, 7.3)),
-        (6.25,36,1,2,7.3,1,1, E12(6.25, 36, 7.3)),
-        (6.25,36,1,3,7.3,1,1, E13(6.25, 36, 7.3)),
-        (6.25,36,2,1,7.3,1,1, E21(6.25, 36, 7.3)),
-        (6.25,36,2,2,7.3,1,1, E22(6.25, 36, 7.3)),
-        (6.25,36,2,3,7.3,1,1, E23(6.25, 36, 7.3)),
-        (6.25,36,2,4,7.3,1,1, E24(6.25, 36, 7.3)),
-        (6.25,36,2,5,7.3,1,1, E25(6.25, 36, 7.3)),
-        (6.25,36,3,1,7.3,1,1, E31(6.25, 36, 7.3)),
-        (6.25,36,3,2,7.3,1,1, E32(6.25, 36, 7.3)),
-        (6.25,36,3,3,7.3,1,1, E33(6.25, 36, 7.3)),
-        (6.25,36,3,4,7.3,1,1, E34(6.25, 36, 7.3)),
-        (6.25,36,3,5,7.3,1,1, E35(6.25, 36, 7.3)),
-        (6.25,36,3,6,7.3,1,1, E36(6.25, 36, 7.3)),
-        (6.25,36,3,7,7.3,1,1, E37(6.25, 36, 7.3)),
-        ]
+    np.random.seed(1234)
+    h2 = np.random.pareto(0.5, size=30)
+    k2 = h2 * (1 + np.random.pareto(0.5, size=h2.size))
+    s = np.random.pareto(0.5, size=h2.size)
+    points = []
+    for i in range(h2.size):
+        for n in range(4):
+            for p in range(1, 2*n+2):
+                points.append((h2[i], k2[i], n, p, s[i]))
+    points = np.array(points)
+    assert_func_equal(ellip_harm, ellip_harm_known, points, rtol = 1e-12)
 
-    data = array(data, dtype=float)
-
-    def w(a, b, c, d, e, f, g):
-        return ellip_harm(a, b, c, d, e, f, g)
-
-    olderr = np.seterr(all='ignore')
-    try:
-        FuncData(w, data, (0,1,2,3,4,5,6), 7, rtol=1e-10, atol=1e-13).check()
-
-    finally:
-        np.seterr(**olderr)
 
